c8bc10c9f82e24300f78c668bce7a093dec6cd1b
youssefbelfeki <youssef.belfeki@gmail.com>
2022-10-19 03:53:04 -0700

    printf

--- functions.c
+++ functions.c
@@ -158,7 +158,8 @@ a[i] = (n / m) % 2;
 }
 for (i = 0, sum = 0, count = 0; i < 32; i++)
 {
-sum += a[i];									if (sum || i == 31)
+sum += a[i];
+if (sum || i == 31)
 {
 char z = '0' + a[i];
 write(1, &z, 1);
